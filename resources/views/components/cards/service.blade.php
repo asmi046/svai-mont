fbfd19648cc70da055f223e9fc76ff1b25a73c0d
asmi046 <asmi046@gmail.com>
2025-06-09 22:53:39 +0300

    Ссылки поправлены

--- resources/views/components/cards/service.blade.php
+++ resources/views/components/cards/service.blade.php
@@ -1,6 +1,6 @@
-<div class="cervice_item">
+<a href="{{ route('service_page', $item->slug) }}" class="cervice_item">
     <div class="order">01.</div>
     <h3 class="title">{{ get_city_text($item->title) }}</h3>
     <p class="subtitle"> {{ get_city_text($item->sub_title) }} </p>
-    <a href="" class="more">Подробнее...</a>
-</div>
+    <span class="more">Подробнее...</span>
+</a>
