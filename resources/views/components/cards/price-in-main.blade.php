e7041da4769fc58714c82aea07af0618a0f076f4
asmi046 <asmi046@gmail.com>
2025-06-24 00:06:10 +0300

    +1

--- resources/views/components/cards/price-in-main.blade.php
+++ resources/views/components/cards/price-in-main.blade.php
@@ -2,8 +2,8 @@
     <p class="label">Винтовые сваи:</p>
     <p class="size">{{$item->size}} мм</p>
     <p class="label">Цена сваи:</p>
-    <p class="price">{{$item->price}} руб.</p>
+    <p class="price">от {{$item->price}} руб.</p>
     <p class="label">Цена с монтажем:</p>
-    <p class="price">{{$item->price_mont}} руб.</p>
+    <p class="price">от {{$item->price_mont}} руб.</p>
     <img class="sv_img" src="{{ Storage::url( $item->img) }}" alt="Цена винтовых свай {{$item->size}} мм">
 </div>
